eb908bc8cc65937b14d5bd3c242bc5ff85208924
sakthi-l <22sakthil64@gmail.com>
2025-06-17 12:59:44 +0530

    Update createdb.py

--- createdb.py
+++ createdb.py
@@ -1,7 +1,14 @@
+import streamlit as st
 from pymongo import MongoClient
+import urllib.parse
 
-uri = "mongodb+srv://SakthiLoganathan:mypassword123@cluster1.gdrhxx1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+username = st.secrets["mongodb"]["username"]
+password = urllib.parse.quote_plus(st.secrets["mongodb"]["password"])  # encode if special chars
+cluster = st.secrets["mongodb"]["cluster"]
+appname = st.secrets["mongodb"]["appname"]
+
+uri = f"mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=majority&appName={appname}"
 
 client = MongoClient(uri)
-db = client['ebooks_db']  # Your database name — create/use ebooks_db
+db = client['ebooks_db']
 collection = db['books']
